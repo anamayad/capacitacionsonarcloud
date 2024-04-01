c4471c81123f0d9c137aefb3805c2d9bd2e52dac
Stiven Avila <stivenavilam@gmail.com>
2024-04-01 12:02:50 -0500

    test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,47 @@
-public class Calculator {
+public class Calculadora {
 
-    public int addition(String arguments) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int sum = 0;
-        for (String add : arguments.split("\\+"))
-            sum += Integer.valueOf(add);
-            return sum;
+        System.out.println("Introduzca el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Introduzca el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Seleccione la operación: ");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+
+        int opcion = scanner.nextInt();
+
+        int resultado = 0;
+
+        switch (opcion) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("No se puede dividir por 0");
+                } else {
+                    resultado = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+
+        if (opcion >= 1 && opcion <= 4) {
+            System.out.println("El resultado de la operación es: " + resultado);
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,58 @@
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class CalculatorTest {
+import static org.junit.Assert.*;
 
-	@Test
-    public void verifyAdditionOne() {
+public class CalculadoraTest {
 
-		Calculator calculator = new Calculator();
-        int sum = calculator.addition("5+2");
-        assertEquals(7, sum);
+    @Test
+    public void testSuma() {
+        int num1 = 5;
+        int num2 = 10;
+        int expectedResult = 15;
+
+        int actualResult = Calculadora.suma(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
     }
-	
-	@Test
-    public void verifyAdditionTwo() {
 
-		Calculator calculator = new Calculator();
-        int sum = calculator.addition("13+27");
-        assertEquals(40, sum);
+    @Test
+    public void testResta() {
+        int num1 = 15;
+        int num2 = 5;
+        int expectedResult = 10;
+
+        int actualResult = Calculadora.resta(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMultiplicacion() {
+        int num1 = 5;
+        int num2 = 4;
+        int expectedResult = 20;
+
+        int actualResult = Calculadora.multiplicacion(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testDivision() {
+        int num1 = 10;
+        int num2 = 2;
+        int expectedResult = 5;
+
+        int actualResult = Calculadora.division(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionPorCero() {
+        int num1 = 10;
+        int num2 = 0;
+
+        Calculadora.division(num1, num2);
     }
 }
